d02c8014ce6135221bfc09ffac031440620aa27c
Himanshu Chaurasiya <himanshuchaurasiya24@gmail.com>
2024-07-27 17:34:28 +0530

    shell_crud_operations_complete

--- home/models.py
+++ home/models.py
@@ -7,7 +7,11 @@ class Student (models.Model):
     age = models.IntegerField()
     email = models.EmailField(null=True, blank=True)
     address = models.TextField(null=True, blank=True)
+    phone = models.TextField(null=True, blank=True)
     # image = models.ImageField()
     # file = models.FileField()
-class Product (models.Model):
-    pass
+class Car(models.Model):
+    car_name = models.CharField(max_length=100)
+    speed = models.IntegerField(default=50)
+    def __str__(self) -> str:
+        return self.car_name
---
+++ home/utils.py
@@ -0,0 +1,7 @@
+from .models import Student
+import time
+def run_this_function():
+    print('Run this functions')
+    time.sleep(1)
+    print('Function exit')
+    # time.sleep(1)
