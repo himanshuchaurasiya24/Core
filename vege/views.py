c91d644b865839cd2087f20812c098ac65754ebb
Himanshu Chaurasiya <himanshuchaurasiya24@gmail.com>
2024-08-07 21:27:43 +0530

    vid at 11:14

--- vege/views.py
+++ vege/views.py
@@ -83,6 +83,11 @@ def register_page(request):
 from django.db.models import Q, Sum
 def get_student(request):
     queryset= Student.objects.all()
+    # ranks = Student.objects.annotate(marks = Sum('studentmarks__marks')).order_by('-marks', 'student_age')
+    # i =1
+    # for rank in ranks:
+    #     print(f'Rank {i} is {rank} with {rank.marks} marks.')
+    #     i+=1
     if request.GET.get('search'):
         search = request.GET.get('search')
         queryset= queryset.filter(
@@ -96,9 +101,17 @@ def get_student(request):
     paginator = Paginator(queryset, 25)
     page_number = request.GET.get('page',1)
     page_obj = paginator.get_page(page_number)
-    print(page_obj)
     return render(request,'report/students.html' ,{'queryset':page_obj})
 def see_marks(request ,student_id):
     queryset = SubjectMarks.objects.filter(student__student_id__student_id=student_id)
     total_marks = queryset.aggregate(total_marks=Sum('marks'))
-    return render(request, 'report/see_result.html', {'queryset':queryset,'total_marks':total_marks})
+    current_rank=-1
+    i =1
+    ranks = Student.objects.annotate(marks = Sum('studentmarks__marks')).order_by('-marks', '-student_age')
+    for rank in ranks:
+        if student_id ==rank.student_id.student_id:
+            current_rank=i
+            break
+        i+=1
+    print(f'Rank is: {current_rank}')
+    return render(request, 'report/see_result.html', {'queryset':queryset,'total_marks':total_marks, 'current_rank':current_rank})
